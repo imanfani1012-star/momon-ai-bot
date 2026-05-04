7335c4ea53cf163777678259d4bf211bc8a1526d
imanfani1012-star <imanfani1012@gmail.com>
2026-05-04 16:30:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 from groq import Groq
 
-TELEGRAM_TOKEN = os.environ.get("8025930923:AAFtdj8UE8kHbF18_5DL7OQI4l0GMJDU0N0")
+TELEGRAM_TOKEN = os.environ.get("8025930923:AAHUDgY_0cJ2MFft78VHBAg4Ma4ocRj-GTw")
 GROQ_KEY = os.environ.get("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")
 
-client = Groq(api_key=GROQ_KEY)
+client = Groq(api_key=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)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Halo bro gue MOMON.AI 🔥 Udah online 24/7 di Railway')
