7a121778e292c33df2ea28b737a2d80de04cedd9
imanfani1012-star <imanfani1012@gmail.com>
2026-05-04 21:39:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def chat(update, context):
         print("USER:", user_message)
 
         response = client.chat.completions.create(
-            model="llama-3.1-70b-versatile",
+            model="llama-3.3-70b-versatile",
             messages=[
                 {"role": "system", "content": "Lu adalah MOMON.AI, bot santai, gaul, dan suka bantu orang."},
                 {"role": "user", "content": user_message}
